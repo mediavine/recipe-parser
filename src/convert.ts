1881f24bad2a1074a673a851dd575e8755ef2dde
ethanbutler <e.thom.butler@gmail.com>
2018-03-12 15:07:20 -0400

    fix typescript errors

--- src/convert.ts
+++ src/convert.ts
@@ -25,31 +25,31 @@ export function getAllMatches(line: string, regex: RegExp) {
   return (matches && [...matches]) || '';
 }
 
-const unicodeObj: { [key: string]: string } = {
-  '¼': '1/4',
-  '½': '1/2',
-  '¾': '3/4',
-  '⅐': '1/7',
-  '⅑': '1/9',
-  '⅒': '1/10',
-  '⅓': '1/3',
-  '⅔': '2/3',
-  '⅕': '1/5',
-  '⅖': '2/5',
-  '⅗': '3/5',
-  '⅘': '4/5',
-  '⅙': '1/6',
-  '⅚': '5/6',
-  '⅛': '1/8',
-  '⅜': '3/8',
-  '⅝': '5/8',
-  '⅞': '7/8'
-};
+// const unicodeObj: { [key: string]: string } = {
+//   '¼': '1/4',
+//   '½': '1/2',
+//   '¾': '3/4',
+//   '⅐': '1/7',
+//   '⅑': '1/9',
+//   '⅒': '1/10',
+//   '⅓': '1/3',
+//   '⅔': '2/3',
+//   '⅕': '1/5',
+//   '⅖': '2/5',
+//   '⅗': '3/5',
+//   '⅘': '4/5',
+//   '⅙': '1/6',
+//   '⅚': '5/6',
+//   '⅛': '1/8',
+//   '⅜': '3/8',
+//   '⅝': '5/8',
+//   '⅞': '7/8'
+// };
 
 export function findQuantityAndConvertIfUnicode(ingredientLine: string) {
   const rangeRegex = /^(\d+)\s?[-–]\s?(\d+)/g;
   const numericAndFractionRegex = /^(\d+\/\d+)|(\d+\s\d+\/\d+)|(\d+\-\d+)|(\d+.\d+)|\d+/g;
-  const unicodeFractionRegex = /\d*[^\u0000-\u007F]+/g;
+  // const unicodeFractionRegex = /\d*[^\u0000-\u007F]+/g;
   if(ingredientLine.match(rangeRegex)) {
     const rangeParts = getAllMatches(ingredientLine, rangeRegex)
     return [rangeParts[0], ingredientLine.replace(rangeRegex, '').trim(), rangeParts[1]]
